056000b742812495348a83d333c1a034b7863735
FelixvL <digitaleservice@gmail.com>
2022-03-25 14:02:58 +0100

    autoboxing wrappers en unboxing

--- Demo.java
+++ Demo.java
@@ -1,17 +1,10 @@
 
 class Demo {  
 	public static void main(String[] args) {
-		Rekenmachine r = new Rekenmachine();
-//		r.optellen(5, 7);
-	//	r.optellen();
-		r.optellen(6,5,4,8,9,1001);
-		int[] getallen = new int[4];
-		r.optellen(getallen);
+		int getal = 6;
+		
+		Integer getal2 = 7;
+		System.out.println(getal2.MAX_VALUE);
 	}
 }
 
-class Rekenmachine{
-	void optellen(int... a) {  // varargs
-		System.out.println(a[3]);
-	}
-}
