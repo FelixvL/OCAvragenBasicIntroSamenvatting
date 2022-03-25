03fbdcc3a5e975328fdd714e4148cdedd6d7814d
FelixvL <digitaleservice@gmail.com>
2022-03-25 13:48:09 +0100

    StringBuilder en String concat append immutable

--- Demo.java
+++ Demo.java
@@ -1,27 +1,17 @@
 
 class Demo {  
 	public static void main(String[] args) {
-		int a = 4;
-//		if(a == checkit()) {
-//			System.out.println("waar");
-//		}
-			// false ||    
-		if(++a == 9 & checkit() == 6) {
-			System.out.println("tweede waar clausule");
-		}
-		// & | ^    bitwise
-		
-		// 10      1010
-		// 3       0011
-		//-------------    1011     0010  1001
-		
-		int r = 10 ^ 3;
-		System.out.println(r);
-	}
-	static int checkit() {
-		System.out.println("we zitten in checkit");
-		return 5;
+		String a = new String("hallo ");
+		a.concat("doei");
+		System.out.println(a);
+		a = a.concat("doei");
+		StringBuilder sba = new StringBuilder("hoi ");
+		sba.append("houdoe");
+		System.out.println(a);
+		System.out.println(sba);
 	}
 }
 
+// hallo
+// hoi houdoe
 
