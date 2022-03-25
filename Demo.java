f0c1c48fe1dcade1abd2f84781d569f36d2fbdd7
FelixvL <digitaleservice@gmail.com>
2022-03-25 13:35:00 +0100

    bitwise operator

--- Demo.java
+++ Demo.java
@@ -1,33 +1,27 @@
 
 class Demo {  
 	public static void main(String[] args) {
-		Fiets f = new Fiets();
-		Fiets f2 = new Fiets();
-		Fiets f3 = new Fiets();
-		Fiets f4 = new Fiets();
+		int a = 4;
+//		if(a == checkit()) {
+//			System.out.println("waar");
+//		}
+			// false ||    
+		if(++a == 9 & checkit() == 6) {
+			System.out.println("tweede waar clausule");
+		}
+		// & | ^    bitwise
+		
+		// 10      1010
+		// 3       0011
+		//-------------    1011     0010  1001
+		
+		int r = 10 ^ 3;
+		System.out.println(r);
 	}
-}
-
-
-class Fiets{
-	Fiets(){
-		System.out.println("Dit is de constructor");
-	}
-	static { /// static initialiser block
-		System.out.println("De klasse fiets wordt geladen");
-	}
-	{
-		System.out.println("Het object fiets wordt geladen");
-	}
-	static int test = vulTest();
-	static int vulTest() {
-		System.out.println("ik zit in vul test");
+	static int checkit() {
+		System.out.println("we zitten in checkit");
 		return 5;
 	}
-	int nietStatisch = vulnietStatisch();
-	int vulnietStatisch() {
-		System.out.println("in vul niet statisch");
-		return 7;
-	}
-
 }
+
+
